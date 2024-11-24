e64df44248b9a3a1efc143cefc67c762e014c79e
kistenklaus <karlsasssie@gmail.com>
2024-11-24 16:15:07 +0100

    partition reference

--- src/wrs/reference/partition.hpp
+++ src/wrs/reference/partition.hpp
@@ -7,33 +7,56 @@
 #include <span>
 #include <tuple>
 #include <vector>
+#include <execution>
+
 namespace wrs::reference {
 
-template <typename T, typename ForwardRange, typename Allocator>
-    requires std::same_as<T, std::ranges::range_value_t<ForwardRange>> &&
-             std::ranges::forward_range<ForwardRange>
+namespace partition {
+using policy = std::execution::par;
+}
+
+template <typename T, typename Allocator>
 std::tuple<std::span<T>, std::span<T>, std::vector<T, Allocator>>
-partition(const ForwardRange& weights, T pivot, const Allocator& alloc) {
+partition(const std::span<T> weights, T pivot, const Allocator& alloc = {}) {
 
     std::vector<T, Allocator> partition(std::ranges::begin(weights), std::ranges::end(weights),
                                         alloc);
     const auto mid = std::partition(partition.begin(), partition.end(),
                                     [pivot](const auto x) { return x > pivot; });
-    return std::make_tuple(std::span<T>(partition.begin(), mid),
-                           std::span<T>(mid, partition.end()), std::move(partition));
+    return std::make_tuple(std::span<T>(partition.begin(), mid), std::span<T>(mid, partition.end()),
+                           std::move(partition));
+}
+
+template <typename T, typename Allocator = std::allocator<T>>
+std::tuple<std::span<T>, std::span<T>, std::vector<T, Allocator>>
+stable_partition(const std::span<T> elements, T pivot, const Allocator& alloc = {}) {
+    std::vector<T, Allocator> partition(elements.begin(), elements.end(), alloc);
+    const auto mid = std::stable_partition(partition.begin(), partition.end(),
+                                           [pivot](const T& x) { return x > pivot; });
+    return std::make_tuple(std::span<T>(partition.begin(), mid), std::span<T>(mid, partition.end()),
+                           std::move(partition));
 }
 
 namespace pmr {
 
-template <typename T, typename ForwardRange>
-    requires std::same_as<T, std::ranges::range_value_t<ForwardRange>> &&
-             std::ranges::forward_range<ForwardRange>
+template <typename T>
 std::tuple<std::span<T>, std::span<T>, std::pmr::vector<T>>
-partition(const ForwardRange& weights, T pivot, const std::pmr::polymorphic_allocator<T>& alloc) {
-    return std::move(wrs::reference::partition<T, ForwardRange, std::pmr::polymorphic_allocator<T>>(
-        weights, pivot, alloc));
+partition(const std::span<T>& weights, T pivot, const std::pmr::polymorphic_allocator<T>& alloc) {
+    return std::move(
+        wrs::reference::partition<T, std::pmr::polymorphic_allocator<T>>(weights, pivot, alloc));
+}
+
+template <typename T>
+std::tuple<std::span<T>, std::span<T>, std::pmr::vector<T>>
+stable_partition(const std::span<T>& elements, T pivot, const std::pmr::polymorphic_allocator<T>& alloc) {
+    return std::move(
+        wrs::reference::stable_partition<T, std::pmr::polymorphic_allocator<T>>(elements, pivot, alloc));
 }
 
 } // namespace pmr
 
 } // namespace wrs::reference
+  //
+  //
+  //
+  //
